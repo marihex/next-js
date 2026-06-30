5cac62509f5ebc645f0386bfd629819460e74089
rieznyk131 <m.rieznyk@gmail.com>
2026-06-30 16:20:30 +0200

    homework-3, add revalidate to the getAllCars fetch

--- src/services/api.services.ts
+++ src/services/api.services.ts
@@ -1,7 +1,7 @@
 import {ICarModel} from "@/src/models/ICarModel";
 
 export const getAllCars = async (): Promise<ICarModel[]> => {
-    const cars = await fetch('http://bigbird.space/carsAPI/v1/cars')
+    const cars = await fetch('http://bigbird.space/carsAPI/v1/cars', {next: {revalidate: 10}})
         .then(value => value.json());
 
     return cars;
